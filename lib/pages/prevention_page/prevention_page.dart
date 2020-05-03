a5d6ecd8870de1440eac0794cecdfcdde2f430ec
Sachin-Liyanage <sachinmamoru@gmail.com>
2020-05-03 22:29:44 +0530

    Prevention page

---
+++ lib/pages/prevention_page/prevention_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PreventionPage extends StatefulWidget {
+  @override
+  _PreventionPageState createState() => new _PreventionPageState();
+}
+
+class _PreventionPageState extends State<PreventionPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Prevention'),
+      ),
+    );
+  }
+}
