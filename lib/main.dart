d9703a0ec8b6a609fc4479cabdd5db71d0cca1b4
Sachin-Liyanage <sachinmamoru@gmail.com>
2020-05-15 11:17:10 +0530

    rotation off

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // SystemChrome.setPreferredOrientations([
-    //   DeviceOrientation.portraitUp,
-    //   DeviceOrientation.portraitDown,
-    // ]);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
